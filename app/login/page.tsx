c3e945064a2dfcf0d4ab2ad2f2184fb05f9bfbb2
nexorabanking <nexorabanking@gmail.com>
2025-06-25 14:30:36 +0100

    push

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -72,7 +72,7 @@ export default function HomePage() {
               <TabsList className="grid w-full grid-cols-2 bg-white/5 backdrop-blur-sm">
                 <TabsTrigger
                   value="login"
-                  className="data-[state=active]:bg-white/10 data-[state=active]:text-white md:data-[state=active]:text-white text-black md:text-white/70"
+                  className="data-[state=active]:bg-black data-[state=active]:text-white md:data-[state=active]:text-white text-black md:text-white/70"
                 >
                   Sign In
                 </TabsTrigger>
